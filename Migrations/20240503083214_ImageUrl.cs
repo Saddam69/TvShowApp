ad8e91d8e49696e45e26dc9dd036dd8c132bcf9c
Saddam69 <92397849+Saddam69@users.noreply.github.com>
2024-05-04 22:43:27 +0530

    Add files via upload

---
+++ Migrations/20240503083214_ImageUrl.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project3.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TvShow",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "TvShow",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "TvShow");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TvShow",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
---
+++ Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project3.Models
+{
+    public enum Genre
+    {
+        Drama,
+        Comedy,
+        Romance,
+        [Display(Name = "Romantic Comedy")]
+        RomCom,
+        Crime,
+        Mystery
+    }
+}
